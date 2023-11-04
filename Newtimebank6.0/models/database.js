04bcedf82aab0a5c85fdaa0ee2cc3fd8cc4034fa
New time bank <banknewtime@gmail.com>
2023-11-04 15:51:33 -0300

    localhost padrao

--- Newtimebank6.0/models/database.js
+++ Newtimebank6.0/models/database.js
@@ -2,14 +2,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-/*   host: 'localhost',
+host: 'localhost',
   user: 'root',
   password: "",
-  database: 'newtimebank', */
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "newtimebank",
+  database: 'newtimebank',
   });
   // waitForConnections: true,
   // connectionLimit: 10,
